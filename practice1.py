1eef82dcb2f0ea2d3bfc5208c668268be5c63988
bhavik321 <49321432+bhavik321@users.noreply.github.com>
2020-11-07 12:03:55 +0530

    Add files via upload

--- practice1.py
+++ practice1.py
@@ -1,4 +1,15 @@
-strq=input()
-s=set(strq) #if we use set, it is same as calculating maximum number of characters in minum length of string
-l=len(s)       
-print(l)
+s=input()
+max_count=0
+for i in range(len(s)):
+	count=1
+	for j in range(i+1,len(str)):
+		str=s[j:count+1]
+		count1=0
+		if(str[i] == str[j]):
+			count1=count1
+		else:
+			count1=count1+1
+	max_count=count1
+	count=count+1
+print(max_count)		
+
